05aae0fca325d5c2d81ffc8049e2a4577a930c07
Rebehk <rebehk.jordao@gmail.com>
2023-11-26 20:49:32 -0300

    add components

---
+++ frontend/books/src/components/Book/index.jsx
@@ -0,0 +1,41 @@
+
+import React from 'react';
+import "./Book.module.css"
+
+export const Book = ({id, titulo, autores, descricao, capa, editora, dataLancamento, qtdPaginas, categoria}) => {
+
+
+    function handleRedirectToBookDetail () {
+
+        const bookDetail = {
+            id,
+            titulo, 
+            descricao,
+            autores,
+            capa,
+            editora,
+            dataLancamento,
+            qtdPaginas,
+            categoria
+        }
+
+        localStorage.setItem('book', JSON.stringify(bookDetail));
+
+        window.location.href = '/bookdetail'
+    }
+
+    return (
+
+        <div onClick={handleRedirectToBookDetail}>
+            <div>
+               <img 
+                    className="capalivro" 
+                    src={capa} alt="Capa do Livro"    
+                />
+                <h2 className='tituloLivro' >{titulo}</h2>
+            </div>
+        </div>
+
+    );
+
+}
